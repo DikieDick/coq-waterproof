9679f32a2431b85311e5b1b7ba40f2bc42439990
DikieDick <26772815+DikieDick@users.noreply.github.com>
2024-05-08 15:38:52 +0200

    Add tests for tactic

---
+++ tests/mathcomp/Tactic.v
@@ -0,0 +1,71 @@
+From mathcomp Require Import eqtype ssrbool ssralg ssrnum.
+
+Import Num.Def Num.Theory.
+
+Require Import Waterproof.Waterproof.
+Require Import Waterproof.Automation.
+Require Import Waterproof.Tactics.
+Require Import Waterproof.Mathcomp.
+
+Waterproof Enable Automation Algebra.
+
+Section eqtype_tests.
+
+Variable T : eqType.
+Variable x y : T.
+
+
+Goal x = y \/ x != y.
+Proof.
+  We conclude that (x = y \/ x != y).
+Qed.
+
+Goal x != y \/ x = y.
+Proof.
+  We conclude that (x != y \/ x = y).
+Qed.
+
+Goal x == y \/ x != y.
+Proof.
+  We conclude that (x == y \/ x != y).
+Qed.
+
+Goal x = y \/ x <> y.
+Proof.
+  We conclude that (x = y \/ x <> y).
+Qed.
+
+Goal x <> y \/ x = y.
+Proof.
+  We conclude that (x <> y \/ x = y).
+Qed.
+
+End eqtype_tests.
+
+Section R_tests.
+Open Scope ring_scope.
+
+Variable R : numDomainType.
+Variable x y : R.
+
+Goal x \is Num.real -> y \is Num.real -> x <= y \/ x > y.
+Proof.
+  Assume that (x \is Num.real).
+  Assume that (y \is Num.real).
+  We conclude that (x <= y \/ x > y).
+Qed.
+
+Goal x \is Num.real -> x > 0 \/ x <= 0.
+Proof.
+  Assume that (x \is Num.real).
+  We conclude that (x > 0 \/ x <= 0).
+Qed.
+
+Goal x \is Num.real -> y \is Num.real -> x < 0 \/ x >= 0.
+Proof.
+  Assume that (x \is Num.real).
+  Assume that (y \is Num.real).
+  We conclude that (x < 0 \/ x >= 0).
+Qed.
+
+End R_tests.
--- theories/Mathcomp/Tactic.v
+++ theories/Mathcomp/Tactic.v
@@ -1,4 +1,4 @@
-From mathcomp Require Import ssreflect ssrbool eqtype ssrfun all_algebra.
+From mathcomp Require Import ssreflect ssrbool eqtype ssrnum.
 
 Import Num.Def Num.Theory.
 
@@ -28,8 +28,6 @@ Qed.
 From Ltac2 Require Import Ltac2 Message.
 
 Ltac2 proof_disjunction_using (lemma: constr) :=
-  print(of_string "We are here");
-  print(of_constr lemma);
   ltac1:(lemma |- case: lemma; try intro; try assumption) (Ltac1.of_constr lemma);
   try (apply or_introl; apply is_true_true);
   try (apply or_introl; assumption);
@@ -49,51 +47,7 @@ Ltac2 proof_disjunction () :=
     proof_disjunction_using constr:(@real_ltgtP) 
   ].
 
-#[export] Hint Extern 1 (_ \/ _) => ltac2:(proof_disjunction ()) : wp_decidability_classical.
-#[export] Hint Extern 1 (_ \/ _ \/ _) => ltac2:(proof_disjunction ()) : wp_decidability_classical.
+#[export] Hint Extern 1 (_ \/ _) => ltac2:(proof_disjunction ()) : wp_core.
+#[export] Hint Extern 1 (_ \/ _ \/ _) => ltac2:(proof_disjunction ()) : wp_core.
 #[export] Hint Resolve eq_sym : wp_core.
 #[export] Hint Resolve neq_sym : wp_core.
-
-(* Goal forall T : eqType, forall x y : T, x = y \/ x != y.
-Proof.
-  intros.
-  auto with wp_decidability_classical.
-Qed.
-
-Goal forall T : eqType, forall x y : T, x != y \/ x = y.
-Proof.
-  intros.
-  auto with wp_decidability_classical.
-Qed.
-
-Goal forall T : eqType, forall x y : T, x == y \/ x != y.
-Proof.
-  intros.
-  auto with wp_decidability_classical.
-Qed.
-
-Goal forall T : eqType, forall x y : T, x = y \/ x <> y.
-Proof.
-  intros.
-  auto with wp_decidability_classical.
-Qed.
-
-Goal forall T : eqType, forall x y : T, x <> y \/ x = y.
-Proof.
-  intros.
-  auto with wp_decidability_classical.
-Qed.
-
-Section R_tests.
-Open Scope ring_scope.
-
-Parameter R : numDomainType.
-Parameters x y : R.
-
-Goal x \is Num.real -> y \is Num.real -> x <= y \/ x > y.
-Proof.
-  intros.
-  auto with wp_decidability_classical.
-Qed.
-
-End R_tests. *)
